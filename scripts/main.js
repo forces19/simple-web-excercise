64e9056ce65ea8a52b1a5a7f4f1a38f8b3d81855
forces19 <singgihajiprasetyo@gmail.com>
2018-02-08 07:32:00 +0700

    change header with input name on prompt when button clicked

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,24 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }
 }
+
+//change header with input name on prompt when button clicked
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
